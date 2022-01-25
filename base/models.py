be319260e06cc76bfaa4c2451e95811c46d26188
Solyaridz3 <solyaridz3@gmail.com>
2022-01-25 12:47:38 +0200

    -registation fix

--- base/models.py
+++ base/models.py
@@ -46,7 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     avatar = models.ImageField(null=True, default="avatar.svg", blank=True)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomUserManager()
 
